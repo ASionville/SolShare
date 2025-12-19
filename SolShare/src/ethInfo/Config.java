9f183c7a27ebe6618114dd2d3a59ee143143f046
ASionville <aubinsionville@gmail.com>
2025-12-19 09:18:43 +0800

    Merged Unix options

--- SolShare/src/ethInfo/Config.java
+++ SolShare/src/ethInfo/Config.java
@@ -7,8 +7,8 @@ public class Config {
 	public static final String pipeLine = "\\\\.\\pipe\\geth.ipc"; // IPC path
 	public static String credential = "Path\\To\\Your\\Ethereum\\keystore\\"; // path to your Ethereum wallet file
 	public static String password = "your_password"; // password for your Ethereum wallet
-
+	
 	// 2. Setting up SolShare Configuration
-	public static final String platform = "Windows"; // Windows, Linux or MacOS
+	public static final String platform = "Windows"; // Windows or Unix
 	public static final boolean useANSIColors = true; // disable if your console does not support ANSI colors
 }
